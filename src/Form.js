670b21944fdbafc1912efd26bb27f61921744b84
LeoSandrini <thesandrini@gmail.com>
2021-08-06 17:45:08 -0300

    Fixing issue and adding comments to explain how to solve it

--- src/Form.js
+++ src/Form.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
 class Form extends React.Component {
+  userNameInput = React.createRef();
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.userNameInput.current.value);
+  }
+
   render() {
     return (
-      <form>
-        <input type="text" name="username" placeholder="Github Username" />
+      //'this' in this context relates to the event scope, not the component
+      //to use this way, you have to bind handleSubmit to the component
+      // <form onSubmit={this.handleSubmit}>
+
+      //Use function like this instead to make sure that the used scope is
+      //the component one
+      <form onSubmit={e => this.handleSubmit(e)}>
+        <input
+          type="text"
+          placeholder="Github Username"
+          // name="username"
+          ref={this.userNameInput}
+          required
+        />
         <button>Add Card</button>
       </form>
     );
